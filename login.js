0efcce1c02bb459554fc9f050b6b42dae39d833e
Esra Başdaş <basdasesra@gmail.com>
2026-02-23 11:47:47 +0300

    Proje taslak yapısı oluşturuldu

---
+++ login.js
@@ -0,0 +1,7 @@
+let information = JSON.parse(localStorage.getItem("userInformation")) || [];
+let editIndex = null;
+
+const name = document.querySelector('#userName');
+const password = document.querySelector('#password');
+
+
